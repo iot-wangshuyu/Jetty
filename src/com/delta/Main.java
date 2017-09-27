2bec1e8ab780ea0a4d8ae77940cbca9cc12614a4
iot_wangshuyu <iot_wangshuyu@126.com>
2017-09-27 09:13:13 +0800

    添加注释

--- src/com/delta/Main.java
+++ src/com/delta/Main.java
@@ -3,8 +3,17 @@ package com.delta;
 
 import com.delta.jetty.JettyServer;
 
+/**
+ * @Title: Main.java 
+ * @Package com.delta 
+ * @Description: TODO
+ * @author Shuyu.Wang
+ * @date Creation time: 2017年9月27日
+ * @version V1.0   
+ */
 public class Main {
 	public static void main(String[] args) {
+		
 		JettyServer.start();
 	}
 }
